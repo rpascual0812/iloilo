50b008c36135c54af87ca6eea0933487f88b0148
Rafael Pascual <rpascual0812@gmail.com>
2014-05-29 11:15:15 +0800

    fixed db credentials

--- functions/connect.php
+++ functions/connect.php
@@ -4,6 +4,6 @@ if($_SERVER['HTTP_HOST'] == "localhost"){
 	or die('Unable to connect to database');
 }
 else{
-    pg_connect("host=localhost port=5432 dbname=d33di4fo1astkk user=zezrzwlldxwnpd password=Mlg2l9Xi62x8RJ9tvQwG-wUEhs")
-	or die('Unable to connect to database');
+    pg_connect("host=ec2-54-83-33-14.compute-1.amazonaws.com port=5432 dbname=d33di4fo1astkk user=zezrzwlldxwnpd password=Mlg2l9Xi62x8RJ9tvQwG-wUEhs sslmode=require options='--client_encoding=UTF8'") 
+    or die('Could not connect: ' . pg_last_error());
 }
