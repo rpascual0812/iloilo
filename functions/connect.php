48d69656f1062896cc078ef11f66e8a09ad1c757
Rafael Pascual <rpascual0812@gmail.com>
2014-05-29 11:00:58 +0800

    added remote connection

--- functions/connect.php
+++ functions/connect.php
@@ -1,4 +1,9 @@
 <?php
-pg_connect("host=localhost port=5432 dbname=photobooth user=photobooth password=photobooth")
-or die('Unable to connect to database');
-?>
+if($_SERVER['HTTP_HOST'] == "localhost"){
+	pg_connect("host=localhost port=5432 dbname=photobooth user=photobooth password=photobooth")
+	or die('Unable to connect to database');
+}
+else{
+    pg_connect("host=localhost port=5432 dbname=d33di4fo1astkk user=zezrzwlldxwnpd password=Mlg2l9Xi62x8RJ9tvQwG-wUEhs")
+	or die('Unable to connect to database');
+}
